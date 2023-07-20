bb81692aeaf1e2470a8bce50cb972f7b03a76e85
Daniil <daniilkukotsky@gmail.com>
2023-07-20 18:45:37 +0400

    Final commit

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int UserNumA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int UserNumB = Convert.ToInt32(Console.ReadLine());
+
+qwe(UserNumA, UserNumB);
+
+int qwe (int numA, int numB)
+{
+    int result = 1;
+    for (int i = 0; i < numB; i++)
+    {
+        result *= numA;
+    }
+    return result;
+}
+
+int res = qwe(UserNumA, UserNumB);
+Console.WriteLine($"Число {UserNumA} в степени {UserNumB} = {qwe(UserNumA, UserNumB)}");
