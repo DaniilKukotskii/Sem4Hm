4b22926a7df7f652cd7cee8feb5508394ad49e60
Daniil <daniilkukotsky@gmail.com>
2023-07-20 18:46:12 +0400

    Final commit

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿int Method (int minDigit, int maxDigit)
+{
+    int count = 8;
+    int i = 0;
+    int [] array = new int[] {};
+    while (i < count)
+    {
+        array[i] = new Random().Next(minDigit, maxDigit + 1);
+        i++;
+    }
+    return Convert.ToInt32(array);
+}
+
+Console.WriteLine("Введите минимальное значение: ");
+int minD = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение: ");
+int maxD = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Массив: [{Method(minD, maxD)}]");
